37e08a17498c40c0bd46649c36adc1dd46a4f8b0
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-10-18 15:50:29 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":app")
+
+include(":feature:home")
+
+include(":core:designsystem")
